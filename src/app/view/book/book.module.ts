2c01af95d33e605f246e04da5583ff1f68b267d0
Oskar Mikus <oskarmikus@protonmail.com>
2021-11-10 18:00:37 +0100

    Add login and register components templates

--- src/app/view/book/book.module.ts
+++ src/app/view/book/book.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {BookComponent} from "./book.component";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     BookComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     CommonModule,
