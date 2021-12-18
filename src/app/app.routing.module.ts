7db8d7bf643a5c2c127cff525b9a84ba095bf52f
Oskar Mikus <oskarmikus@protonmail.com>
2021-12-18 13:52:06 +0100

    Add home page component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookComponent } from './view/book/book.component';
+import { HomeComponent } from './view/home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: BookComponent },
   { path: '', pathMatch: 'full', redirectTo: 'BookComponent' },
 ];
 
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly turnOnOff = faPowerOff;
+}
