86e30f55f2cbe4ac6c956d2ca4cf1d7a2735295f
Oskar Mikus <oskarmikus@protonmail.com>
2022-01-04 15:48:34 +0100

    Add card component template

--- src/app/pkg/components/components.module.ts
+++ src/app/pkg/components/components.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { ToastComponent } from './toast/toast.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {CardComponent} from "./card/card.component";
 
 @NgModule({
-  declarations: [LoaderComponent, ToastComponent],
+  declarations: [LoaderComponent, ToastComponent, CardComponent],
   imports: [CommonModule, FontAwesomeModule],
   providers: [],
-  exports: [LoaderComponent, ToastComponent],
+  exports: [LoaderComponent, ToastComponent, CardComponent],
 })
 export class ComponentsModule {}
