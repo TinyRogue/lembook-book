6318a4c72e4cbf0123059e5cc14e875a554f1ff4
Oskar Mikus <oskarmikus@protonmail.com>
2022-01-29 16:55:29 +0100

    heroku connect

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -7,7 +7,7 @@ const prodURI = 'https://lembook-serv.herokuapp.com/gql';
 const devURI = 'http://localhost:8080/query';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({ uri: devURI, withCredentials: true }),
+    link: httpLink.create({ uri: prodURI, withCredentials: true }),
     cache: new InMemoryCache(),
     credentials: 'include',
   };
